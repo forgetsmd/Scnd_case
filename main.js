453c64591630079eccac3506a63495bc816056d9
forgetsmd <forgetsmd@gmail.com>
2020-10-25 20:35:26 +0300

    Fifth commit

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-console.log("Hallo, World!");
-const sum = (a, b) => a + b;
-const dif = (a, b) => a - b;
-const mult = (a, b) => a * b;
-const div = (a, b) => a / b;
-
+function ban(){
+    console.log("Hallo, World!");
+    const sum = (a, b) => a + b;
+    const dif = (a, b) => a - b;
+    const mult = (a, b) => a * b;
+    const div = (a, b) => a / b;
+    console.log(sum(1,2));
+    console.log(dif(3,4));
+    console.log(mult(5,6));
+    console.log(div(7,8));
+}
+ban();
